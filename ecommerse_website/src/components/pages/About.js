9655ab239c9a518b58fc0fc7bff9fd0ec16aca59
ruchitakb7 <ruchitakb7@gmail.com>
2024-10-30 01:21:00 +0530

    about-footer page

--- ecommerse_website/src/components/pages/About.js
+++ ecommerse_website/src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React ,{Fragment}from "react"
-import { Container,Image } from "react-bootstrap"
+import { Container,Image, Row ,Col} from "react-bootstrap"
 import pic from "../../assets/Band Members.png"
 import Footer from "../UI/Footer"
 
@@ -19,7 +19,34 @@ The Generics
 </p>
            </div>
         </Container>
-        <Footer></Footer>
+        <Footer>
+            <Container className="d-flex align-items-center ">
+                <Row className=" w-100 align-items-center justify-content-center ">
+                    <Col className="align-items-center justify-content-center " md={3}>
+                  
+                    <a href="https://www.youtube.com" target="_blank" className="text-white ms-5 me-3">
+                                <Image src="https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
+                                            alt="YouTube Logo"
+                                            style={{ width: '30px', height: '30px' }}/>
+                            </a>
+                    
+                            <a href="https://spotify.com" target="_blank" className="text-white ms-4 me-4">
+                                <Image src="https://prasadyash2411.github.io/ecom-website/img/Spotify Logo.png" alt=""
+                                 style={{ width: '30px', height: '30px' }} />
+                            </a>
+                       
+              
+                    
+                            <a href="https://facebook.com" target="_blank" className="text-white ms-3">
+                                <Image src="https://prasadyash2411.github.io/ecom-website/img/Facebook Logo.png" alt=""
+                                 style={{ width: '30px', height: '30px' }} />
+                            </a>
+                    </Col>
+                 
+                </Row>
+
+            </Container>
+        </Footer>
         </Fragment>
         
     )
